e6c83eec7d455fc0fd9977d14ff9080695af5e5d
arafdewann <arafdewan100@gmail.com>
2025-04-13 05:40:23 -0400

    Add Vercel config and custom error pages (404 & 500)

---
+++ pages/500.js
@@ -0,0 +1,8 @@
+export default function Custom500() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>500 – Server Error</h1>
+      <p>Oops! Something went wrong. Please try again later.</p>
+    </div>
+  );
+}
